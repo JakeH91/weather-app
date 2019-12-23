1e9bb828df787ff66228b24e58fab506e59470d7
Jake <jakereh91@gmail.com>
2019-12-23 23:02:56 +0000

    use darksky api to log current weather

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-  console.log('2 Second Timer')
-}, 2000);
+const url = 'https://api.darksky.net/forecast/69a34aa5c7f8c47e51feac9b644c2d36/37.8267,-122.4233?units=si&exclude=hourly,minutely,flags,offset';
 
-setTimeout(() => {
-  console.log('0 Second Timer')
-}, 0)
+request({ url: url, json: true }, (error, response) => {
+  const { currently, daily } = response.body;
 
-console.log('Stopping');
+  console.log(`${daily.data[0].summary} It is currently ${currently.temperature} degrees out. There is a ${currently.precipProbability}% chance of rain.`);
+})
